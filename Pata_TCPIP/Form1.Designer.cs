256a2c1d921a221696dec3f38a05dbbb9d9b8bf8
Quentin <win.moi@hotmail.com>
2020-10-19 09:26:47 +0200

    Ajoutez des fichiers projet.

---
+++ Pata_TCPIP/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Pata_TCPIP
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuCommunication = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationClient = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationClientEcouter = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationClientConnecter = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationUdp = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationUdpEcouter = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationUdpConnecter = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationSocket = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationSocketEcouter = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationSocketConnecter = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuCommunicationSocketDeconnecter = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuUtilitaire = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuUtilitaireVerifier = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuQuitter = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.labelServeur = new System.Windows.Forms.Label();
+			this.textBoxServeur = new System.Windows.Forms.TextBox();
+			this.textBoxMessage = new System.Windows.Forms.TextBox();
+			this.labelMessage = new System.Windows.Forms.Label();
+			this.buttonEnvoyer = new System.Windows.Forms.Button();
+			this.labelEchange = new System.Windows.Forms.Label();
+			this.listBoxEchange = new System.Windows.Forms.ListBox();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCommunication,
+            this.menuUtilitaire,
+            this.menuQuitter});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(415, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// menuCommunication
+			// 
+			this.menuCommunication.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCommunicationClient,
+            this.menuCommunicationUdp,
+            this.menuCommunicationSocket});
+			this.menuCommunication.Name = "menuCommunication";
+			this.menuCommunication.Size = new System.Drawing.Size(106, 20);
+			this.menuCommunication.Text = "Communication";
+			// 
+			// menuCommunicationClient
+			// 
+			this.menuCommunicationClient.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCommunicationClientEcouter,
+            this.menuCommunicationClientConnecter});
+			this.menuCommunicationClient.Name = "menuCommunicationClient";
+			this.menuCommunicationClient.Size = new System.Drawing.Size(180, 22);
+			this.menuCommunicationClient.Text = "Listener / Client";
+			// 
+			// menuCommunicationClientEcouter
+			// 
+			this.menuCommunicationClientEcouter.Name = "menuCommunicationClientEcouter";
+			this.menuCommunicationClientEcouter.Size = new System.Drawing.Size(129, 22);
+			this.menuCommunicationClientEcouter.Text = "Ecouter";
+			this.menuCommunicationClientEcouter.Click += new System.EventHandler(this.menuCommunicationClientEcouter_Click);
+			// 
+			// menuCommunicationClientConnecter
+			// 
+			this.menuCommunicationClientConnecter.Name = "menuCommunicationClientConnecter";
+			this.menuCommunicationClientConnecter.Size = new System.Drawing.Size(129, 22);
+			this.menuCommunicationClientConnecter.Text = "Connecter";
+			this.menuCommunicationClientConnecter.Click += new System.EventHandler(this.menuCommunicationClientConnecter_Click);
+			// 
+			// menuCommunicationUdp
+			// 
+			this.menuCommunicationUdp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCommunicationUdpEcouter,
+            this.menuCommunicationUdpConnecter});
+			this.menuCommunicationUdp.Name = "menuCommunicationUdp";
+			this.menuCommunicationUdp.Size = new System.Drawing.Size(180, 22);
+			this.menuCommunicationUdp.Text = "UDP";
+			// 
+			// menuCommunicationUdpEcouter
+			// 
+			this.menuCommunicationUdpEcouter.Name = "menuCommunicationUdpEcouter";
+			this.menuCommunicationUdpEcouter.Size = new System.Drawing.Size(129, 22);
+			this.menuCommunicationUdpEcouter.Text = "Ecouter";
+			this.menuCommunicationUdpEcouter.Click += new System.EventHandler(this.menuCommunicationUdpEcouter_Click);
+			// 
+			// menuCommunicationUdpConnecter
+			// 
+			this.menuCommunicationUdpConnecter.Name = "menuCommunicationUdpConnecter";
+			this.menuCommunicationUdpConnecter.Size = new System.Drawing.Size(129, 22);
+			this.menuCommunicationUdpConnecter.Text = "Connecter";
+			this.menuCommunicationUdpConnecter.Click += new System.EventHandler(this.menuCommunicationUdpConnecter_Click);
+			// 
+			// menuCommunicationSocket
+			// 
+			this.menuCommunicationSocket.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCommunicationSocketEcouter,
+            this.menuCommunicationSocketConnecter,
+            this.menuCommunicationSocketDeconnecter});
+			this.menuCommunicationSocket.Name = "menuCommunicationSocket";
+			this.menuCommunicationSocket.Size = new System.Drawing.Size(180, 22);
+			this.menuCommunicationSocket.Text = "Socket";
+			// 
+			// menuCommunicationSocketEcouter
+			// 
+			this.menuCommunicationSocketEcouter.Name = "menuCommunicationSocketEcouter";
+			this.menuCommunicationSocketEcouter.Size = new System.Drawing.Size(180, 22);
+			this.menuCommunicationSocketEcouter.Text = "Ecouter";
+			this.menuCommunicationSocketEcouter.Click += new System.EventHandler(this.menuCommunicationSocketEcouter_Click);
+			// 
+			// menuCommunicationSocketConnecter
+			// 
+			this.menuCommunicationSocketConnecter.Name = "menuCommunicationSocketConnecter";
+			this.menuCommunicationSocketConnecter.Size = new System.Drawing.Size(180, 22);
+			this.menuCommunicationSocketConnecter.Text = "Connecter";
+			this.menuCommunicationSocketConnecter.Click += new System.EventHandler(this.menuCommunicationSocketConnecter_Click);
+			// 
+			// menuCommunicationSocketDeconnecter
+			// 
+			this.menuCommunicationSocketDeconnecter.Enabled = false;
+			this.menuCommunicationSocketDeconnecter.Name = "menuCommunicationSocketDeconnecter";
+			this.menuCommunicationSocketDeconnecter.Size = new System.Drawing.Size(180, 22);
+			this.menuCommunicationSocketDeconnecter.Text = "Déconnecter";
+			this.menuCommunicationSocketDeconnecter.Click += new System.EventHandler(this.menuCommunicationSocketDeconnecter_Click);
+			// 
+			// menuUtilitaire
+			// 
+			this.menuUtilitaire.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuUtilitaireVerifier});
+			this.menuUtilitaire.Name = "menuUtilitaire";
+			this.menuUtilitaire.Size = new System.Drawing.Size(63, 20);
+			this.menuUtilitaire.Text = "Utilitaire";
+			// 
+			// menuUtilitaireVerifier
+			// 
+			this.menuUtilitaireVerifier.Name = "menuUtilitaireVerifier";
+			this.menuUtilitaireVerifier.Size = new System.Drawing.Size(110, 22);
+			this.menuUtilitaireVerifier.Text = "Vérifier";
+			this.menuUtilitaireVerifier.Click += new System.EventHandler(this.menuUtilitaireVerifier_Click);
+			// 
+			// menuQuitter
+			// 
+			this.menuQuitter.Name = "menuQuitter";
+			this.menuQuitter.Size = new System.Drawing.Size(56, 20);
+			this.menuQuitter.Text = "Quitter";
+			this.menuQuitter.Click += new System.EventHandler(this.menuQuitter_Click);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// labelServeur
+			// 
+			this.labelServeur.AutoSize = true;
+			this.labelServeur.Location = new System.Drawing.Point(12, 24);
+			this.labelServeur.Name = "labelServeur";
+			this.labelServeur.Size = new System.Drawing.Size(44, 13);
+			this.labelServeur.TabIndex = 2;
+			this.labelServeur.Text = "Serveur";
+			// 
+			// textBoxServeur
+			// 
+			this.textBoxServeur.Location = new System.Drawing.Point(15, 40);
+			this.textBoxServeur.Name = "textBoxServeur";
+			this.textBoxServeur.Size = new System.Drawing.Size(388, 20);
+			this.textBoxServeur.TabIndex = 3;
+			this.textBoxServeur.Text = "3700X";
+			// 
+			// textBoxMessage
+			// 
+			this.textBoxMessage.Location = new System.Drawing.Point(15, 97);
+			this.textBoxMessage.Name = "textBoxMessage";
+			this.textBoxMessage.Size = new System.Drawing.Size(388, 20);
+			this.textBoxMessage.TabIndex = 5;
+			// 
+			// labelMessage
+			// 
+			this.labelMessage.AutoSize = true;
+			this.labelMessage.Location = new System.Drawing.Point(12, 81);
+			this.labelMessage.Name = "labelMessage";
+			this.labelMessage.Size = new System.Drawing.Size(50, 13);
+			this.labelMessage.TabIndex = 4;
+			this.labelMessage.Text = "Message";
+			// 
+			// buttonEnvoyer
+			// 
+			this.buttonEnvoyer.Location = new System.Drawing.Point(15, 138);
+			this.buttonEnvoyer.Name = "buttonEnvoyer";
+			this.buttonEnvoyer.Size = new System.Drawing.Size(388, 38);
+			this.buttonEnvoyer.TabIndex = 6;
+			this.buttonEnvoyer.Text = "Envoyer";
+			this.buttonEnvoyer.UseVisualStyleBackColor = true;
+			this.buttonEnvoyer.Click += new System.EventHandler(this.buttonEnvoyer_Click);
+			// 
+			// labelEchange
+			// 
+			this.labelEchange.AutoSize = true;
+			this.labelEchange.Location = new System.Drawing.Point(12, 200);
+			this.labelEchange.Name = "labelEchange";
+			this.labelEchange.Size = new System.Drawing.Size(55, 13);
+			this.labelEchange.TabIndex = 7;
+			this.labelEchange.Text = "Echanges";
+			// 
+			// listBoxEchange
+			// 
+			this.listBoxEchange.FormattingEnabled = true;
+			this.listBoxEchange.Location = new System.Drawing.Point(15, 216);
+			this.listBoxEchange.Name = "listBoxEchange";
+			this.listBoxEchange.Size = new System.Drawing.Size(388, 225);
+			this.listBoxEchange.TabIndex = 8;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(415, 450);
+			this.Controls.Add(this.listBoxEchange);
+			this.Controls.Add(this.labelEchange);
+			this.Controls.Add(this.buttonEnvoyer);
+			this.Controls.Add(this.textBoxMessage);
+			this.Controls.Add(this.labelMessage);
+			this.Controls.Add(this.textBoxServeur);
+			this.Controls.Add(this.labelServeur);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "TCP/IP";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunication;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationClient;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationClientEcouter;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationClientConnecter;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationUdp;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationUdpEcouter;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationUdpConnecter;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationSocket;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationSocketEcouter;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationSocketConnecter;
+		private System.Windows.Forms.ToolStripMenuItem menuCommunicationSocketDeconnecter;
+		private System.Windows.Forms.ToolStripMenuItem menuUtilitaire;
+		private System.Windows.Forms.ToolStripMenuItem menuUtilitaireVerifier;
+		private System.Windows.Forms.ToolStripMenuItem menuQuitter;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.Label labelServeur;
+		private System.Windows.Forms.TextBox textBoxServeur;
+		private System.Windows.Forms.TextBox textBoxMessage;
+		private System.Windows.Forms.Label labelMessage;
+		private System.Windows.Forms.Button buttonEnvoyer;
+		private System.Windows.Forms.Label labelEchange;
+		private System.Windows.Forms.ListBox listBoxEchange;
+	}
+}
+
---
+++ Pata_TCPIP/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pata_TCPIP
+{
+	public partial class Form1 : Form
+	{
+		private Socket _serveur, _client;
+		private int _socketFlag; // 1 pour le serveur, 2 client
+		private byte[] _buffer = new byte[256];
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void menuUtilitaireVerifier_Click(object sender, EventArgs e)
+		{
+			if (textBoxServeur.Text.Length > 0)
+			{
+				IPAddress ipServeur = AdresseValide(textBoxServeur.Text);
+
+				if (ipServeur != null)
+					MessageBox.Show(@"Ping réussi");
+				else
+					MessageBox.Show(@"Pas de ping");
+			}
+			else
+				MessageBox.Show(@"Veuillez entrez un serveur !");
+		}
+
+		/**
+		 * <summary>Récupère la première adresse IPv4 valide</summary>
+		 */
+		private IPAddress AdresseValide(string nomPc)
+		{
+			IPAddress ipReponse = null;
+
+			if (nomPc.Length > 0)
+			{
+				IPAddress[] ipServeur = Dns.GetHostEntry(nomPc).AddressList;
+
+				for (int i = 0; i < ipServeur.Length; i++)
+				{
+					Ping pingServeur = new Ping();
+					PingReply pingReponse = pingServeur.Send(ipServeur[i]);
+
+					if (pingReponse != null && pingReponse.Status == IPStatus.Success)
+					{
+						if (ipServeur[i].AddressFamily == AddressFamily.InterNetwork) // si addresse ipv4
+						{
+							ipReponse = ipServeur[i];
+
+							break;
+						}
+					}
+				}
+			}
+
+			return ipReponse;
+		}
+
+		private void menuQuitter_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void menuCommunicationClientEcouter_Click(object sender, EventArgs e)
+		{
+			menuCommunicationClientEcouter.Enabled = false;
+
+			IPAddress ipLocal = AdresseValide(Dns.GetHostName()); // récupère l'adresse du serveur
+
+			TcpListener listenerServeur = new TcpListener(ipLocal, 8000); // config du serveur (son ip et son port)
+			listenerServeur.Start(); // attente en bloquant
+
+			TcpClient client = listenerServeur.AcceptTcpClient(); // accepte une connexion client
+			NetworkStream flux = client.GetStream(); // récupère le flux du client
+
+			BinaryWriter binaryWriter = new BinaryWriter(flux); // crée la réponse du serveur
+			binaryWriter.Write(@"Connexion initialisée"); // écrit la réponse
+
+			BinaryReader binaryReader = new BinaryReader(flux); // lit la réponse du client
+			listBoxEchange.Items.Add( binaryReader.ReadString()); // récupère ce qui est envoyé par le client
+
+			binaryWriter.Write(@"Ordre de déconnexion"); // indique que le serveur va fermer la connexion
+
+			listBoxEchange.Items.Add(binaryReader.ReadString()); // récupère la réponse du client
+
+			// ferme les connexions
+			client.Close();
+			listenerServeur.Stop();
+
+			menuCommunicationClientEcouter.Enabled = true;
+		}
+
+		private void menuCommunicationClientConnecter_Click(object sender, EventArgs e)
+		{
+			menuCommunicationClientEcouter.Enabled = false;
+
+			IPAddress ipServeur = AdresseValide(textBoxServeur.Text); // récupération de l'ip du serveur
+			TcpClient client = new TcpClient(); // création du client
+
+			client.Connect(ipServeur, 8000); // connexion au serveur
+
+			NetworkStream flux = client.GetStream();
+			BinaryReader binaryReader = new BinaryReader(flux); // récupère le flux du serveur
+			listBoxEchange.Items.Add(binaryReader.ReadString()); // lit ce flux
+
+			BinaryWriter binaryWriter = new BinaryWriter(flux); // crée la réponse au serveur
+			binaryWriter.Write(@"Machine " + AdresseValide(Dns.GetHostName()) + " connectée"); // écrit la réponse indiquant qui est le client
+			listBoxEchange.Items.Add(binaryReader.ReadString()); // lit ce flux
+
+			binaryWriter.Write(@"Déconnexion effectuée"); // indique que le sclient s'est bien déconnecté
+
+			client.Close();
+
+			menuCommunicationClientEcouter.Enabled = true;
+		}
+
+		private async void menuCommunicationUdpEcouter_Click(object sender, EventArgs e)
+		{
+			await ServeurAsync();
+		}
+
+		private Task ServeurAsync()
+		{
+			return Task.Run(ExecuteServer);
+		}
+
+		private void ExecuteServer()
+		{
+			// création serveur
+			IPAddress ipLocal = AdresseValide(Dns.GetHostName()); // récupère l'adresse du serveur
+			IPEndPoint ipEndPoint = new IPEndPoint(ipLocal, 8000);
+
+			UpdateUI("Attente de clients...");
+
+			UdpClient serveur = new UdpClient(ipEndPoint); // config du serveur (son ip et son port)
+
+			// réception données
+			IPEndPoint client = new IPEndPoint(IPAddress.Any, 0);
+			var data = serveur.Receive(ref client);
+			UpdateUI("Message : " + Encoding.ASCII.GetString(data));
+
+			// envoie réponse
+			UpdateUI("Envoie du message : Bienvenue sur le serveur");
+			data = Encoding.ASCII.GetBytes("Bienvenue sur le serveur");
+			serveur.Send(data, data.Length, client);
+
+			UpdateUI("Fermeture du serveur");
+
+			// fermeture
+			serveur.Close();
+		}
+
+		private void UpdateUI(string text)
+		{
+			if (InvokeRequired) // permet de lancer cette méthode via un autre thread
+			{
+				BeginInvoke((MethodInvoker)delegate
+				{
+					UpdateUI(text);
+				});
+				return;
+			}
+
+			listBoxEchange.Items.Add(text);
+		}
+
+		private void menuCommunicationUdpConnecter_Click(object sender, EventArgs e)
+		{
+			// This constructor arbitrarily assigns the local port number.
+			UdpClient udpClient = new UdpClient(6000);
+			try
+			{
+				listBoxEchange.Items.Add("Connexion au serveur...");
+				IPAddress ipLocal = AdresseValide(Dns.GetHostName()); // récupère l'adresse du serveur
+				udpClient.Connect(ipLocal, 8000);
+
+				// Sends a message to the host to which you have connected.
+				listBoxEchange.Items.Add("Envoie du message : Coucou serveur");
+				var sendBytes = Encoding.ASCII.GetBytes("Coucou serveur");
+				udpClient.Send(sendBytes, sendBytes.Length);
+
+				//IPEndPoint object will allow us to read datagrams sent from any source.
+				IPEndPoint serveur = new IPEndPoint(IPAddress.Any, 0);
+
+				// Blocks until a message returns on this socket from a remote host.
+				Byte[] receiveBytes = udpClient.Receive(ref serveur);
+				string returnData = Encoding.ASCII.GetString(receiveBytes);
+				listBoxEchange.Items.Add("Réception du message : " + returnData);
+
+				listBoxEchange.Items.Add("Fermeture de la connexion");
+				udpClient.Close();
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.ToString());
+			}
+		}
+
+		private void menuCommunicationSocketEcouter_Click(object sender, EventArgs e)
+		{
+			menuCommunicationSocketEcouter.Enabled = false; // désactive le menu
+			menuCommunicationSocketDeconnecter.Enabled = true; // permet de se déconnecter
+
+			_socketFlag = 1;
+			_client = null; // reset le client
+			IPAddress ipServeur = AdresseValide(Dns.GetHostName()); // traduit le nom du pc en ip
+
+			// socket ipv4, flux en tcp
+			_serveur = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			_serveur.Bind(new IPEndPoint(ipServeur, 8000)); // associe le socket à notre ip et au port
+			_serveur.Listen(1); // accepte au plus 1 client
+			_serveur.BeginAccept(OnConnectionDemand, _serveur); // fonction à exécuter lors d'une connexion sur ce serveur
+		}
+
+		private void OnConnectionDemand(IAsyncResult asyncResult)
+		{
+			if(_socketFlag != 1) return; // si ce n'est pas le serveur
+
+			Socket temp = (Socket) asyncResult.AsyncState;
+
+			_client = temp.EndAccept(asyncResult); // accepte la connexion
+			_client.Send(Encoding.Unicode.GetBytes("Connexion acceptée"));
+
+			UpdateUI("Connexion effectuée par " + ((IPEndPoint) _client.RemoteEndPoint).Address);
+
+			InitReception(_client);
+		}
+
+		private void InitReception(Socket socket)
+		{
+			_buffer = new byte[256];
+			// recoit dans le buffer de l'emplacement 0 à sa longueur sans flag particulier, reception traitera ce socket
+			socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, Reception, socket);
+		}
+
+		private void Reception(IAsyncResult asyncResult)
+		{
+			if (_socketFlag < 1) return; // si le flag est bien défini
+
+			Socket temp = (Socket)asyncResult.AsyncState;
+			int recu = temp.EndReceive(asyncResult); // retourne le nombre d'octets recus
+
+			if (recu > 0)
+			{
+				UpdateUI(Encoding.Unicode.GetString(_buffer));
+				InitReception(temp); // prépare la prochaine réception de message
+			}
+			else
+			{
+				temp.Disconnect(true); // déconnecte le socket
+				temp.Close(); // ferme la connexion
+				_serveur.BeginAccept(OnConnectionDemand, _serveur); // attend une nouvelle connexion
+				_client = null; // reset le client
+			}
+		}
+
+		private void menuCommunicationSocketConnecter_Click(object sender, EventArgs e)
+		{
+			if (textBoxServeur.Text.Length < 1)
+			{
+				MessageBox.Show(@"Renseigner le serveur");
+				return;
+			}
+
+			menuCommunicationSocketConnecter.Enabled = false; // désactive le menu
+			menuCommunicationSocketDeconnecter.Enabled = true; // permet de se déconnecter
+
+			_socketFlag = 2;
+			// socket ipv4, flux en tcp
+			_client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			_client.Blocking = false; // empêche le socket de bloquer le thread
+			IPAddress serveur = AdresseValide(textBoxServeur.Text); // récupère l'ip du serveur
+
+			// établit au connexion au serveur et call back sur OnConnection
+			_client.BeginConnect(new IPEndPoint(serveur, 8000), OnConnection, _client);
+		}
+
+		private void OnConnection(IAsyncResult asyncResult)
+		{
+			Socket temp = (Socket)asyncResult.AsyncState; // récupère le socket
+
+			if (temp.Connected) // si la connexion a bien été établie
+				InitReception(temp);
+			else
+				MessageBox.Show(@"Serveur inaccessible");
+		}
+
+		private void menuCommunicationSocketDeconnecter_Click(object sender, EventArgs e)
+		{
+			if (_socketFlag == 2) // si c'est le client
+			{
+				_client.Send(Encoding.Unicode.GetBytes("Déconnexion client"));
+
+				_client.Shutdown(SocketShutdown.Both); // ferme la connexion du client ET du serveur
+
+				// réinit les données
+				_socketFlag = 0;
+				menuCommunicationSocketConnecter.Enabled = true; // active le menu
+				menuCommunicationSocketDeconnecter.Enabled = false; // permet de se déconnecter
+
+				// demande de se déconnecter via la méthode callback OnDemandeDeconnect
+				_client.BeginDisconnect(false, OnDemandeDeconnect, _client);
+			}
+			else if (_client == null) // si c'est le serveur
+			{
+				_serveur.Close();
+
+				// réinit les données
+				_socketFlag = 0;
+				menuCommunicationSocketEcouter.Enabled = true; // active le menu
+				menuCommunicationSocketDeconnecter.Enabled = false; // permet de se déconnecter
+			}
+			else
+				MessageBox.Show(@"Client connecté = pas de déconnexion");
+		}
+
+		private void OnDemandeDeconnect(IAsyncResult asyncResult)
+		{
+			Socket temp = (Socket) asyncResult.AsyncState; // récupère le socket
+			temp.EndDisconnect(asyncResult); // termine la déconnexion
+		}
+
+		private void buttonEnvoyer_Click(object sender, EventArgs e)
+		{
+			// envoie le message écrit
+			_client.Send(Encoding.Unicode.GetBytes(textBoxMessage.Text));
+		}
+	}
+}
